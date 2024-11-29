79c7069be04414f0605a4bb52520969e5aaa0d76
Evgeniya ISHKINA <e.ishkina@net.estia.fr>
2024-11-29 12:05:21 +0100

    Added comments

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ app = FastAPI()
 class Item(BaseModel):
     name: str
     price: PositiveFloat
-    tax: PositiveFloat | None = Field(None, gt=0.0, lt=1.0)
+    # different options for fields on models:
+    # https://docs.pydantic.dev/latest/api/fields/
+    # lt: If set, value must be less than this. Only applicable to numbers.
+    tax: PositiveFloat | None = Field(None, lt=1.0)
 
     @computed_field
     def price_with_tax(self) -> float:
         coef_tax = (1 + self.tax) if self.tax else 1
         return self.price * coef_tax
 
-
+# Initialized every time the server is run
 items = {
     1: Item(name="item 1", price=1.0, tax=0.2),
     2: Item(name="item 2", price=3.0),
@@ -31,7 +34,7 @@ def root():
 
 @app.get("/items/list")
 def read_items():
-    return {"items": items}
+    return items
 
 
 @app.get("/items/{item_id}")
@@ -44,6 +47,7 @@ def read_item(item_id: int):
 
 
 @app.put("/items/{item_id}")
+# item is a request payload
 def update_item(item_id: int, item: Item):
 
     if item_id not in items:
@@ -56,7 +60,8 @@ def update_item(item_id: int, item: Item):
 @app.post("/items/")
 async def create_item(item: Item):
 
-    item_id = max(items.keys()) + 1 if len(items) !=0 else 1
+    # define id for the new item
+    item_id = max(items.keys()) + 1 if len(items) != 0 else 1
     items.update({item_id: item})
     return {"item_id": item_id, "item": item}
 
